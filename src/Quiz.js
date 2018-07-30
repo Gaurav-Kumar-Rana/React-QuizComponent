b4c0fb39f0d5f1ce0a2a162a48c0e75f3819a688
Gaurav-Kumar-Rana <32073848+Gaurav-Kumar-Rana@users.noreply.github.com>
2018-07-30 18:20:01 +0530

    Module 4
    Quize End implemented.

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
+const quiz_position = quizData.quiz_questions.length - 1;
 
 class Quiz extends Component {
   constructor(props){
     super(props);
     this.state = {
-      quiz_position:1
+      quiz_position:1,
+      isQuizEnd:false
     }
   }
   render(){
     return(
-      <div>
+      <div className={this.state.isQuizEnd?'hide':'show'} >
         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
       </div>
       )
