b6b86e3a31adcc78429ed854f5bc1fe11cbae1af
Rana <Gaurav.Rana@gartner.com>
2018-07-31 13:01:49 +0530

    Module 7
    Resetting the Quiz.

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,6 +16,12 @@ class Quiz extends Component {
       quiz_position:this.state.quiz_position + 1,
     });
   }
+  handleResetClick(){
+    this.props.resetClickHandler();
+    this.setState({
+      quiz_position:1,
+    });
+  }
   render(){
      const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length ? true:false;
     return(
